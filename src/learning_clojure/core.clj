65eb2c31f52c5d5016b687d011a622b1e9c3ed72
Diego Felix <diegoflx.oliveira@gmail.com>
2019-08-07 23:21:03 -0300

    add dependencies to core

--- src/learning_clojure/core.clj
+++ src/learning_clojure/core.clj
@@ -1,6 +1,5 @@
-(ns learning-clojure.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns learning-clojure.core
+  ;; The `:as` keyword is the same `as` in PHP.
+  ;; Useful to use only jetty/some-function instead of calling
+  ;; the full namespace (e.g: ring.adapter.jetty/some-function)
+  (:require [ring.adapter.jetty :as jetty]))
