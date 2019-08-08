a0101f5260dfaeb67de04601310bda19eeb68f09
Diego Felix <diegoflx.oliveira@gmail.com>
2019-08-07 23:20:48 -0300

    add call to main on project

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
   :license {:name "Creative Commons Attribution Share-Alike 4.0 International"
             :url "https://creativecommons.org"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.7.1"]])
+                 [ring "1.7.1"]]
+
+  ;; This code will call a function called `-main` on core as soon as
+  ;; we call `lein run`.
+  ;; Useful when we want to run the code automatically.
+  :main learning-clojure.core)
