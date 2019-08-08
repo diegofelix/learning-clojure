3143f1344aecd22452dd6907639c8a76ec45d071
Diego Felix <diegoflx.oliveira@gmail.com>
2019-08-07 22:58:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# learning-clojure
+
+Hi, I'm learning clojure and create this repo following a Tutorial.
+Please, access the [tutorial website](https://practicalli.github.io).
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject learning-clojure "0.1.0-SNAPSHOT"
+  :description "Simple project to learn clojure."
+  :url "http://github.com/diegofelix/learning-clojure"
+  :license {:name "Creative Commons Attribution Share-Alike 4.0 International"
+            :url "https://creativecommons.org"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
